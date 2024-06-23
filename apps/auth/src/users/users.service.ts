f7e12afe2f9adc3e6b69dcd380ff6cea2aff5cfd
bootspace <gregudogu@gmail.com>
2024-06-23 23:45:04 +0100

    Initial Commit

---
+++ apps/auth/src/users/users.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { UserRepository } from './user.repository';
+import { CreateUserDTO } from './dto/create-user.dto';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  async create(createUserDto: CreateUserDTO) {
+    return this.userRepository.create({ ...createUserDto });
+  }
+}
